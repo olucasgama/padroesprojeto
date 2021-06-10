3f6d71f22150a927c539f201fb6170fc97edcf86
Lucas Santos <42410626+olucasgama@users.noreply.github.com>
2021-06-10 08:42:58 -0300

    v1.1

---
+++ padroescriacao/src/padroescriacao/singleton/main/Produto.java
@@ -0,0 +1,84 @@
+package padroescriacao.singleton.main;
+
+public class Produto {
+    
+    private Produto() {};
+    private static Produto instance = new Produto();
+    public static Produto getInstance(){
+        return instance;
+    }
+    
+    private int id;
+    private String nome;
+    private String descricao;
+    private String especificacoes;
+    private String marca;
+    private int codigoBarra;
+    private int quantidade;
+    private float preco;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEspecificacoes() {
+        return especificacoes;
+    }
+
+    public void setEspecificacoes(String especificacoes) {
+        this.especificacoes = especificacoes;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getCodigoBarra() {
+        return codigoBarra;
+    }
+
+    public void setCodigoBarra(int codigoBarra) {
+        this.codigoBarra = codigoBarra;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    
+}
